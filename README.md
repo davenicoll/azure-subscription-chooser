73a54fb60decaedc7fccdf2b22156f62e2bae7e5
Dave Nicoll <690117+davenicoll@users.noreply.github.com>
2024-05-03 10:28:25 -0700

    shellcheck tweaks and updated readme

--- README.md
+++ README.md
@@ -20,6 +20,8 @@ if [ -z $SUBSCRIPTION ]; then echo "You chose nothing!"; else; echo "You chose $
 
 `whiptail` is required to create the menu, as most distros have it installed already. https://en.wikibooks.org/wiki/Bash_Shell_Scripting/Whiptail
 
+`jq` is required to process the json response from `az`.
+
 #### Install whiptail on macOS
 
 `whiptail` isn't installed on macOS, but can be installed with `brew` (https://command-not-found.com/whiptail) and the package is called `newt`.
--- subscription-chooser.sh
+++ subscription-chooser.sh
@@ -6,28 +6,28 @@ MENU=""
 IFS=$(echo -en "\n\b")
 ITEMS=0
 for SUBCRIPTION in $(az account list --query "sort_by([], &name) | [?state=='Enabled'].[name, id, isDefault]" --all | jq -rc '.[] | @csv'); do
-    NAME=$(echo $SUBCRIPTION | awk -F',' '{print $1}' | tr -d \")
-    ID=$(echo $SUBCRIPTION | awk -F',' '{print $2}' | tr -d \")
-    IS_DEFAULT=$(echo $SUBCRIPTION | awk -F',' '{print $3}' | tr -d \")
+    NAME=$(echo "${SUBCRIPTION}" | awk -F',' '{print $1}' | tr -d \")
+    ID=$(echo "${SUBCRIPTION}" | awk -F',' '{print $2}' | tr -d \")
+    # IS_DEFAULT=$(echo ${SUBCRIPTION} | awk -F',' '{print $3}' | tr -d \")
 
-    NAME=$(printf '%-70s' "$NAME")
+    NAME=$(printf '%-70s' "${NAME}")
 
-    MENU+="$NAME ($ID)\n  \n"
+    MENU+="${NAME} (${ID})\n  \n"
     ITEMS=$((ITEMS + 1))
 done
 
 export NEWT_COLORS=''
-RESULT=$(whiptail --title "Azure Subscriptions" --menu "" $((ITEMS + 8)) 120 $ITEMS $(echo -e $MENU) 3>&1 1>&2 2>&3)
+RESULT=$(whiptail --title "Azure Subscriptions" --menu "" $((ITEMS + 8)) 120 ${ITEMS} $(echo -e "${MENU}") 3>&1 1>&2 2>&3)
 
-SUBSCRIPTION_ID=$(echo $RESULT | sed 's/.*(\(.*\))/\1/' | sed 's/\s*$//g')
-if [ -z $SUBSCRIPTION_ID ]; then exit; fi
+SUBSCRIPTION_ID=$(echo "${RESULT}" | sed 's/.*(\(.*\))/\1/' | sed 's/\s*$//g')
+if [ -z "${SUBSCRIPTION_ID}" ]; then exit; fi
 
 if [[ -t 1 ]]; then
     # executed directly, set the subscription
-    az account set --subscription $SUBSCRIPTION_ID
+    az account set --subscription "${SUBSCRIPTION_ID}"
     echo ""
     az account show --query "{name:name, subscriptionId:id, tenantId:tenantId}" --output table --only-show-errors
 else
     # executed by another script, return the subscription id
-    echo $SUBSCRIPTION_ID
+    echo "${SUBSCRIPTION_ID}"
 fi
